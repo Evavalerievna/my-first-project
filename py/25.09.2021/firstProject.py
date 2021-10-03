193ae1023a10e70c3d7f458b394432cef0cf2de5
Tatiana <time-86@mail.ru>
2021-10-03 09:18:10 +0200

    Добавлены лекции и домашние работы

---
+++ py/25.09.2021/firstProject.py
@@ -0,0 +1,78 @@
+# name = "Tatiana"
+# print("hello, ", name)
+# a = d = c = 1
+# print(a, d, c)
+
+# a = 1
+# b = 2
+#
+# print('a:', a)
+# print('b:', b)
+# c = b
+# b = a
+# a = c
+# print(a, b)
+
+# s1 = "Выполнил задание"
+# s2 = "Багинская Т. В. "
+# s3 = "слушатель группы python 111"
+# print("\"Выполнил задание\n\t\t Багинская Т. В.\n\t\t\t\t слушатель группы python 111\" ")
+
+# a = 5
+# b = 7
+# c = 3
+# print(a + b + c)
+# print(a * b * c)
+# print((a + b + c)/3)
+
+# a = "9"
+# b = "7"
+# c = "5"
+# d = "3"
+# print("Исходное число:", a + b + c + d)
+# print("Обратное число:", d + c + b + a)
+
+# num = 9876
+# res = (num % 10) * 1000
+# # print(res)
+# num = num // 10
+# # print(num)
+# res += (num % 10) * 100
+# # print(res)
+# num = num // 10
+# # print(num)
+# res += (num % 10) * 10
+# # print(res)
+# num = num // 10
+# # print(num)
+# res += num % 10
+# print(res)
+
+# num1 = "2"
+# num2 = 3
+# res = int(num1) + num2
+# print(res)
+
+# print(int(3.8))  # 3
+# print(round(3.8))  # 4
+# print(round(3.891, 2))  # 3.89
+
+# Домашнее задание. Поменять местами переменные
+# Вариант 1
+# a = 1
+# b = 2
+# a, b = b, a
+# print(a)
+# print(b)
+
+# Вариант 2
+# a = 1
+# b = 2
+# a = a + b   # 3
+# b = a - b   # 1
+# a = a - b
+#
+# print(a)
+# print(b)
+
+
